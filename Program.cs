d6e216f34409b041ccf10c5b864769a7874a1719
Bence Pásztor <bence7661@gmail.com>
2025-07-14 22:07:08 +0200

    Reworks service registrator.

--- Program.cs
+++ Program.cs
@@ -1,37 +1,14 @@
 using Microsoft.EntityFrameworkCore;
-using Microsoft.OpenApi.Models;
 using NoteTakerAPI;
 using NoteTakerAPI.Data;
-using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<NotesDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-ServiceRegistrator.RegisterServices(builder.Services);
-
-if (builder.Environment.IsDevelopment())
-{
-    builder.Services.AddSwaggerGen(options =>
-    {
-        options.SwaggerDoc("v1", new OpenApiInfo
-        {
-            Title = "NoteTaker API",
-            Version = "v1",
-            Description = "API for my React practice project",
-            Contact = new OpenApiContact
-            {
-                Name = "Serious",
-                Email = "canthavemy@email.com"
-            }
-        });
-
-        var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-        var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-        options.IncludeXmlComments(xmlPath, includeControllerXmlComments: true);
-    });
-}
+var serviceRegistrator = new ServiceRegistrator(builder);
+serviceRegistrator.RegisterServices();
 
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
@@ -39,6 +16,7 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseCors("AllowAllDebug");
 }
 
 app.UseHttpsRedirection();
--- ServiceRegistrator.cs
+++ ServiceRegistrator.cs
@@ -1,16 +1,56 @@
-﻿using NoteTakerAPI.Repositories;
+﻿using Microsoft.OpenApi.Models;
+using NoteTakerAPI.Repositories;
+using System.Reflection;
 
 namespace NoteTakerAPI;
 
-public static class ServiceRegistrator
+public class ServiceRegistrator(WebApplicationBuilder builder)
 {
-    public static void RegisterServices(IServiceCollection services)
+    public void RegisterServices()
     {
-        services.AddControllers();
-        services.AddOpenApi();
+        builder.Services.AddControllers();
+        builder.Services.AddOpenApi();
 
-        services.AddScoped<INotebookRepository, NotebookRepository>();
-        services.AddScoped<INoteRepository, NoteRepository>();
-        services.AddScoped<IUserRepository, UserRepository>();
+        builder.Services.AddScoped<INotebookRepository, NotebookRepository>();
+        builder.Services.AddScoped<INoteRepository, NoteRepository>();
+        builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+        RegisterDevOnlyServices();
+    }
+
+    private void RegisterDevOnlyServices()
+    {
+        if (builder.Environment.IsDevelopment())
+        {
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "NoteTaker API",
+                    Version = "v1",
+                    Description = "API for my React practice project",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Serious",
+                        Email = "canthavemy@email.com"
+                    }
+                });
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                options.IncludeXmlComments(xmlPath, includeControllerXmlComments: true);
+            });
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAllDebug", policy =>
+                {
+                    policy
+                        .AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader();
+                });
+            });
+        }
     }
 }
